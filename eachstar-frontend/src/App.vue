3c5973ba54cade343d2842d65c75272c9e25be48
FTLIKON <854892057@qq.com>
2022-09-08 00:02:31 +0800

    vue helloworld

--- eachstar-frontend/src/App.vue
+++ eachstar-frontend/src/App.vue
@@ -1,23 +1,17 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
-</template>
+<!--
+跟 Vue 说 Hello World！
+-->
 
 <script>
 export default {
-  name: 'App'
-}
+  data() {
+    return {
+      message: "Hello World!"
+    };
+  }
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <h1>{{ message }}</h1>
+</template>
