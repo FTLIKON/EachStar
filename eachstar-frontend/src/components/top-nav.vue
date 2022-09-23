7403c3e3ad8ad0ed703997bbb6716f5a5b551ed3
Nesb01t <littlelumos@foxmail.com>
2022-09-23 21:30:21 +0800

    update: 美化border

--- eachstar-frontend/src/components/top-nav.vue
+++ eachstar-frontend/src/components/top-nav.vue
@@ -5,13 +5,17 @@
     @select="menuSelect"
   >
     <el-menu-item index="1">
-      <router-link to="/github" class="link">发现仓库</router-link>
+      <router-link to="/github" class="link">欢迎加入</router-link>
     </el-menu-item>
     <el-menu-item index="2">
+      <router-link to="/github" class="link">发现仓库</router-link>
+    </el-menu-item>
+    <el-menu-item index="3">
       <router-link to="/test" class="link">你的仓库</router-link>
     </el-menu-item>
 
     <el-button @click="authButton()" id="auth-button">注册 / 登录</el-button>
+    <div id="logo">EachStar</div>
   </el-menu>
   <authPage ref="authPage" />
 </template>
@@ -31,21 +35,35 @@ export default {
 </script>
 
 <style>
-.flex-grow {
-  flex-grow: 3;
-}
-
 .link {
+  font-weight: bold;
   color: white;
   text-decoration: none;
 }
 
 #auth-button {
+  font-weight: bold;
   color: white;
-  background-color: #3c8dbc;
+  background-color: #337ecc;
   
-  height: auto;
-  margin-right: 1%;
+  margin: auto 1%;
   margin-left: auto;
+
+  border: none;
+  border-radius: 10px;
+  text-shadow: 0px 0px 1px #303133; 
+}
+
+#logo {
+  font-weight: bold;
+  font-size: x-large;
+  color: white;
+  
+  margin-right: 1%;
+  margin-left: 1%;
+  margin-top: auto;
+  margin-bottom: auto;
+
+  text-shadow: 1px 1px 2px #303133, 0px 0px 1px #303133; 
 }
 </style>
