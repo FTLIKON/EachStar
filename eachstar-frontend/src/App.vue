0daf315f037d46585f3dcae4814671ceb698a3f2
Nesb01t <littlelumos@foxmail.com>
2022-09-25 18:34:22 +0800

    add: 去掉top-nav测试

--- eachstar-frontend/src/App.vue
+++ eachstar-frontend/src/App.vue
@@ -1,15 +1,12 @@
 <template>
   <div id="app">
-    <top-nav />
     <router-view />
   </div>
 </template>
 
 <script>
-import TopNav from "./components/top-nav.vue";
 export default {
   components: {
-    "top-nav": TopNav,
   },
 };
 </script>
