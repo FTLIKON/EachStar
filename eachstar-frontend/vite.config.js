475a7dba7aeeb44ca251487d4795475f7d64e263
libin <ubuntu@localhost.localdomain>
2022-09-25 18:10:31 +0800

    update: gengx

--- eachstar-frontend/vite.config.js
+++ eachstar-frontend/vite.config.js
@@ -10,5 +10,11 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 12377,
+    // open: true, //在服务器启动时自动在浏览器中打开应用程序
+    // 反向代理配置，注意rewrite写法
+  },
 })
