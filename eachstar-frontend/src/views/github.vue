c8b1c4d1b689ba7b9e425e258ecf98b944564184
Nesb01t <littlelumos@foxmail.com>
2022-09-25 22:35:40 +0800

    update: try proxy

--- eachstar-frontend/src/views/github.vue
+++ eachstar-frontend/src/views/github.vue
@@ -116,7 +116,7 @@ export default {
           that.totalCard = parseInt(response.data.count);
           that.totalPage = Math.ceil(that.totalCard/10);
           that.cardData = response.data.data;
-          console.log(that.cardData.value);
+          console.log(that.cardData.Proxy[0]);
         })
         .catch(function (error) {
           console.log(error);
