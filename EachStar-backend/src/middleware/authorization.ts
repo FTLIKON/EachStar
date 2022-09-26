ce55a00dbac79514ae36e07cc2e26e8cf210cf23
FTLIKON <854892057@qq.com>
2022-09-26 17:30:10 +0800

    update: user id await

--- EachStar-backend/src/middleware/authorization.ts
+++ EachStar-backend/src/middleware/authorization.ts
@@ -3,12 +3,16 @@ import { RepositoryPostgres } from '../stores'
 export const authorization = async (ctx: Context, next: Next) => {
   const repository = new RepositoryPostgres()
   const userId = ctx.cookies.get('userId')
-  if (userId != undefined) {
-    const user = await repository.getUserById(BigInt(userId))
-    console.log(user)
+  const userName = ctx.cookies.get('userName')
+  console.log(userId, userName)
+  if (userId != undefined && userName != undefined) {
+    let user = await repository.getUserById(BigInt(userId))
     if (user) {
       ctx.user = user
       ctx.cookies.set('userId', userId, { httpOnly: false })
+    } else {
+      user = await repository.createUser(BigInt(userId), userName, BigInt(0))
+      ctx.user = user
     }
   }
 
