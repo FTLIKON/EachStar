015377b19fed7499cd5f54eb4b1051cd7683adbb
Nesb01t <littlelumos@foxmail.com>
2022-09-26 22:02:41 +0800

    fix: complete ExpireTime

--- eachstar-frontend/src/components/public.vue
+++ eachstar-frontend/src/components/public.vue
@@ -120,7 +120,7 @@ export default {
       if(hour<10)hour="0"+hour;
       if(minute<10)minute="0"+minute;
       if(second<10)second="0"+second;
-      return year+"-"+month+"-"+date+"   "+hour+":"+minute+":"+second;
+      return year+"-"+month+"-"+date+" "+hour+":"+minute+":"+second;
     },
   },
 }
