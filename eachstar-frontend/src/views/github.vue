905c1b792c9f402dc0aec7e4c49839b79c948617
Nesb01t <littlelumos@foxmail.com>
2022-09-26 14:26:14 +0800

    update: rankNum func

--- eachstar-frontend/src/views/github.vue
+++ eachstar-frontend/src/views/github.vue
@@ -8,6 +8,7 @@
           <div class="card-discription">{{i.context}} {{i.updatedAt}}</div>
           <div class="card-valueblock">
             <span class="card-rank">积分价值{{i.starPrice}}</span>
+            <span class="card-rank-num">悬赏次数{{i.starNum}}</span>
             <el-button id="card-button" @click="starButton(i.title)" type="success" plain>Star</el-button>
           </div>
         </div>
@@ -196,6 +197,18 @@ export default {
 }
 .card-rank {
   color: #b88230;
+  background-color: none;
+  
+  width: 80%;
+  margin-left: 3%;
+  margin-right: 3%;
+  margin-top: auto;
+  margin-bottom: auto;
+  text-align: left;
+  font-size: small;
+}
+.card-rank-num {
+  color: #337ecc;
   background-color: none;
   
   width: 80%;
