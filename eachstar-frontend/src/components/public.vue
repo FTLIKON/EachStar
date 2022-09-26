333ff6c9ce83a9408c2e1317c68e48bcf8e6bbd2
Nesb01t <littlelumos@foxmail.com>
2022-09-26 15:14:26 +0800

    update: remove bkg color&add user rank show

--- eachstar-frontend/src/components/public.vue
+++ eachstar-frontend/src/components/public.vue
@@ -14,24 +14,21 @@
         type="textarea"
         clearable/>
       <div class="rank-setting-block">
-        <span>积分价值: {{ cardRank }}
-          <el-slider
-          v-model="cardRank"
-          class="slider"
-          step="1"
-          :min="1"
-          :max="5"></el-slider>
-        </span>
-
-        <span>
-          悬赏次数: {{ cardRankNum }}
-          <el-slider
-          v-model="cardRankNum"
-          class="slider"
-          step="1"
-          :min="1"
-          :max="50"></el-slider>
-        </span>
+        积分价值: {{ cardRank }}<el-slider
+        v-model="cardRank"
+        class="slider"
+        step="1"
+        :min="1"
+        :max="5"></el-slider>
+        悬赏次数: {{ cardRankNum }}<el-slider
+        v-model="cardRankNum"
+        class="slider"
+        step="1"
+        :min="1"
+        :max="50"></el-slider>
+      </div>
+      <div class="user-rank-calc">
+        消耗积分: {{ cardRank*cardRankNum }} <el-divider direction="vertical" /> 剩余积分: 123
       </div>
       <span class="dialog-footer">
         <el-button @click="dialogVisible = false">取消</el-button>
@@ -91,7 +88,6 @@ export default {
   margin-bottom: 10px;
 }
 .rank-setting-block {
-  background-color: aquamarine;
   width: 70%;
 }
 .slider {
