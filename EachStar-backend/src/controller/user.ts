e5041fc4a56cfc6894f624283406c0e30789509c
FTLIKON <854892057@qq.com>
2022-09-26 17:40:16 +0800

    update: change user price api

--- EachStar-backend/src/controller/user.ts
+++ EachStar-backend/src/controller/user.ts
@@ -7,13 +7,19 @@ export class UserController {
     this.repository = new RepositoryPostgres()
   }
 
-  
-
   async getSelf(ctx: Context) {
     ctx.assert(ctx.user, 403, 'user not found', {
       code: 'USER_NOT_FOUND',
     })
     ctx.body = ctx.user
+  }
+
+  async changeUserPrice(ctx: Context) {
+    const body = ctx.request.body
+    const userId = ctx.user.id
+    const newPrice = body.newPrice
+    const user = this.repository.changeUserPrice(userId, newPrice)
+    ctx.body = user
   }
 }
 export default UserController
