120c7c2ff7cbbe6f4171f9bb2ad714d528047c61
Nesb01t <littlelumos@foxmail.com>
2022-09-27 15:05:30 +0800

    add: star empty icon

--- eachstar-frontend/src/views/github.vue
+++ eachstar-frontend/src/views/github.vue
@@ -13,12 +13,12 @@
               <span style="color: #409EFF">悬赏次数{{i.starNum}}</span> 
             </span>
             <el-button 
-              v-show="i.starred"
+              v-show="!i.starred"
               id="card-button"
               @click="starButton(i)"
-              plain><span id="star-false">☆</span> Star</el-button>
+              plain><span id="star-false">✩</span> Star</el-button>
             <el-button
-              v-show="!i.starred"
+              v-show="i.starred"
               id="card-button"
               type="info"
               plain><span id="star-true">⭐</span> Starred</el-button>
