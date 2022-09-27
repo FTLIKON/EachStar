d4c968d6a6db1d7834ceb4fcbf017742fca5d03d
Nesb01t <littlelumos@foxmail.com>
2022-09-27 15:00:46 +0800

    fix: double star

--- eachstar-frontend/src/views/github.vue
+++ eachstar-frontend/src/views/github.vue
@@ -16,9 +16,9 @@
               v-show="!i.starred"
               id="card-button"
               @click="starButton(i)"
-              plain><span id="star-false">★</span>☆ Star</el-button>
+              plain><span id="star-false">☆</span> Star</el-button>
             <el-button
-              v-show="i.starred"
+              v-show="!i.starred"
               id="card-button"
               type="info"
               plain><span id="star-true">★</span> Starred</el-button>
