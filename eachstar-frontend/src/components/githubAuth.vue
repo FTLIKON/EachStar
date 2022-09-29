5a31ccee0eba36fa5c92888902902ac6256c8555
Nesb01t <littlelumos@foxmail.com>
2022-09-29 14:22:56 +0800

    update: change githubAuth css

--- eachstar-frontend/src/components/githubAuth.vue
+++ eachstar-frontend/src/components/githubAuth.vue
@@ -2,12 +2,13 @@
   <el-dialog v-model="dialogVisible" title="github快捷登录">
     <el-button
       type="primary"
+      class="link-button"
       @click="
         goPage(
           'http://119.91.192.183:3050/oauth/github/login?path=http://119.91.192.183:80/'
         )
       "
-      >点我</el-button
+      >使用github快捷登陆”</el-button
     >
   </el-dialog>
 </template>
@@ -32,25 +33,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.dialog-footer {
-  display: flex;
-  width: 100%;
-  justify-content: center;
-  margin-top: 20px;
-  .el-button:first-child {
-    margin-right: 25px;
-  }
-}
-
-.password-inputs {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-
-  .el-input {
-    width: 70%;
-    margin-top: 10px;
-  }
-}
 </style>
