cac09a4c078bcd2d85d986f676f433255b9ec8dd
Nesb01t <littlelumos@foxmail.com>
2022-09-29 15:01:34 +0800

    fix: commit link

--- eachstar-frontend/src/components/topNav.vue
+++ eachstar-frontend/src/components/topNav.vue
@@ -19,7 +19,7 @@
       <el-divider direction="vertical"/>
       <el-menu-item index="2">
         <router-link to="/mygithub" class="link" v-show="isLogin">你的仓库</router-link>
-        <router-link @click="noLoginError()" class="link" v-show="!isLogin">你的仓库</router-link>
+        <!-- <router-link @click="noLoginError()" class="link" v-show="!isLogin">你的仓库</router-link> -->
       </el-menu-item>
     </div>
 
@@ -60,7 +60,6 @@ export default {
       menuSelect(key, keyPath) {
         console.log(key);
       },
-
       noLoginError() {
         ElMessage({
           message: "请先进行登录!",
