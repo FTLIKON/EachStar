e7848cad5c849eedd2981383651f3b6537ef0b0f
Nesb01t <littlelumos@foxmail.com>
2022-10-01 12:40:38 +0800

    update: try get time

--- eachstar-frontend/src/views/github.vue
+++ eachstar-frontend/src/views/github.vue
@@ -8,7 +8,7 @@
             item.title.replace("https://github.com/", "")
           }}</a>
           <div class="card-discription">
-            {{ item.context }} {{ item.updatedAt }}
+            {{ item.context }} {{ item.updatedAt.getTime() }}
           </div>
           <div class="card-valueblock">
             <span class="card-rank">
