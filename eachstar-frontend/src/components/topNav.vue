5824b200a8b90a58d226c06f64f4143057ae19c1
FTLIKON <854892057@qq.com>
2022-10-01 15:12:57 +0800

    update：button color

--- eachstar-frontend/src/components/topNav.vue
+++ eachstar-frontend/src/components/topNav.vue
@@ -194,7 +194,8 @@ export default {
   font-family: "微软雅黑";
   margin-left: 10%;
   margin-right: 10%;
-
+  width: 10%;
+  height: 10%;
   border-radius: 12px;
   background-color: white;
   color: black;
