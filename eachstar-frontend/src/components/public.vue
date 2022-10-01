88acf01d2e56b86374508a12d59859dbe1e62095
Nesb01t <littlelumos@foxmail.com>
2022-10-01 12:28:42 +0800

    update: 发布检查

--- eachstar-frontend/src/components/public.vue
+++ eachstar-frontend/src/components/public.vue
@@ -63,8 +63,7 @@ export default {
           message: "您输入的似乎不是Github链接, 请检查",
           type: "warning",
         });
-      }
-      if (this.userPrice-this.starPrice*this.starNum < 0){
+      } else if (this.userPrice-this.starPrice*this.starNum < 0){
         ElMessage({
           message: "您当前积分不足~ 快去star别人的卡片吧!",
           type: "warning",
