46cef74d662c4da592a2f103db897b0e8c9090db
FTLIKON <854892057@qq.com>
2022-10-01 13:15:04 +0800

    update: card time

--- eachstar-frontend/src/views/github.vue
+++ eachstar-frontend/src/views/github.vue
@@ -114,11 +114,11 @@ export default {
 
       axios(config)
         .then(function (response) {
-          ElMessage({
-            message:
-              "请确认您的github仓库是否公开, 不公开其他人无法给您star哦~",
-            type: "success",
-          });
+          // ElMessage({
+          //   message:
+          //     "请确认您的github仓库是否公开, 不公开其他人无法给您star哦~",
+          //   type: "success",
+          // });
           that.getPageData(0);
           bus.emit("refreshUserInfo");
         })
