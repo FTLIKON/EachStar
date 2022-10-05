d0da0cf39224e939cdb63b9af70339ef71553187
FTLIKON <854892057@qq.com>
2022-10-05 17:31:30 +0800

    update：user icon

--- eachstar-frontend/src/components/topNav.vue
+++ eachstar-frontend/src/components/topNav.vue
@@ -60,53 +60,21 @@
         >
         <span v-show="isLogin" class="user-hello">您好, {{ userName }}</span>
 
-
         <el-popover
           :width="300"
           popper-style="box-shadow: rgb(14 18 22 / 35%) 0px 10px 38px -10px, rgb(14 18 22 / 20%) 0px 10px 20px -15px; padding: 20px;"
         >
           <template #reference>
             <el-avatar
-
               v-show="isLogin"
               :src="userIconURL"
               src="https://avatars.githubusercontent.com/u/72015883?v=4"
             />
           </template>
           <template #default>
-            <div
-              class="demo-rich-conent"
-              style="display: flex; gap: 16px; flex-direction: column"
-            >
-              <el-avatar
-                :size="60"
-                src="https://avatars.githubusercontent.com/u/72015883?v=4"
-                style="margin-bottom: 8px"
-              />
-              <div>
-                <p
-                  class="demo-rich-content__name"
-                  style="margin: 0; font-weight: 500"
-                >
-                  Element Plus
-                </p>
-                <p
-                  class="demo-rich-content__mention"
-                  style="
-                    margin: 0;
-                    font-size: 14px;
-                    color: var(--el-color-info);
-                  "
-                >
-                  @element-plus
-                </p>
-              </div>
-
-              <p class="demo-rich-content__desc" style="margin: 0">
-                Element Plus, a Vue 3 based component library for developers,
-                designers and product managers
-              </p>
-            </div>
+            <el-button id="card-button" type="danger" plain>
+              退出登录
+            </el-button>
           </template>
         </el-popover>
       </div>
