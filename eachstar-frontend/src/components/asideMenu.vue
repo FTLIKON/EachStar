0ea13e73d5843fe65e1b848d60cd3ea4a5e379e8
Nesb01t <littlelumos@foxmail.com>
2022-10-06 13:18:54 +0800

    update: change to primary price button

--- eachstar-frontend/src/components/asideMenu.vue
+++ eachstar-frontend/src/components/asideMenu.vue
@@ -68,24 +68,13 @@ export default {
   font-size: larger;
   font-weight: bolder;
   text-shadow: 1px 1px 2px #303133, 0px 0px 2px #303133;
-
   border-radius: 15px;
   box-shadow: var(--el-box-shadow-dark);
 
   --el-button-text-color: var(--el-color-white);
-  --el-button-bg-color: var(--el-color-success);
-  --el-button-border-color: var(--el-color-success);
-  --el-button-outline-color: var(--el-color-success-light-5);
-  --el-button-active-color: var(--el-color-success-dark-2);
-  --el-button-hover-text-color: var(--el-color-white);
-  --el-button-hover-link-text-color: var(--el-color-success-light-5);
-  --el-button-hover-bg-color: var(--el-color-success-light-3);
-  --el-button-hover-border-color: var(--el-color-success-light-3);
-  --el-button-active-bg-color: var(--el-color-success-dark-2);
-  --el-button-active-border-color: var(--el-color-success-dark-2);
-  --el-button-disabled-text-color: var(--el-color-white);
-  --el-button-disabled-bg-color: var(--el-color-success-light-5);
-  --el-button-disabled-border-color: var(--el-color-success-light-5);
+  --el-button-bg-color: var(--el-color-primary);
+  --el-button-border-color: var(--el-color-primary);
+  --el-button-outline-color: var(--el-color-primary-light-5);
 
   vertical-align: middle;
   background-color: var(--el-button-bg-color);
@@ -96,7 +85,6 @@ export default {
   justify-content: center;
   align-items: center;
   line-height: 1;
-  height: 32px;
   color: var(--el-button-text-color);
   text-align: center;
   box-sizing: border-box;
