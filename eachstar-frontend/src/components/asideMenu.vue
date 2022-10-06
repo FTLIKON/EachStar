50c9eeb0f95f6ae7434262fde3aea37157b9a596
Nesb01t <littlelumos@foxmail.com>
2022-10-06 13:12:51 +0800

    update: 去掉v-show

--- eachstar-frontend/src/components/asideMenu.vue
+++ eachstar-frontend/src/components/asideMenu.vue
@@ -3,7 +3,7 @@
     <el-button class="public-button" @click="openPublicDialog()" type="success">
       发布卡片
     </el-button>
-    <span v-show="isLogin" class="user-rank">
+    <span class="user-rank">
       可用星币: {{ userPrice }}
     </span>
     <PublicDialog ref="PublicDialog" />
