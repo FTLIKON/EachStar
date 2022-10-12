dd107f0e85f03345b2b778c49d001d92614dc3e6
FTLIKON <854892057@qq.com>
2022-10-12 15:00:28 +0800

    update: backend

--- EachStar-backend/src/app.ts
+++ EachStar-backend/src/app.ts
@@ -14,6 +14,19 @@ BigInt.prototype.toJSON = function () {
 }
 
 const app = new Koa()
+app.use(async (ctx, next) => {
+  ctx.set('Access-Control-Allow-Origin', '*')
+  ctx.set(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild',
+  )
+  ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS')
+  if (ctx.method == 'OPTIONS') {
+    ctx.body = 200
+  } else {
+    await next()
+  }
+})
 
 app.use(koaBody())
 app.use(authorization)
