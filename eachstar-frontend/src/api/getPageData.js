79b7735c4f167a592a72eb5c2203faa9c7e14f74
Nesb01t <littlelumos@foxmail.com>
2022-10-12 22:53:56 +0800

    update: page

--- eachstar-frontend/src/api/getPageData.js
+++ eachstar-frontend/src/api/getPageData.js
@@ -15,7 +15,7 @@ export async function getPageData(type, page) {
     };
     var res = await axios(config);
     
-    console.log(res);
+    console.log(page);
     var count = res.data.count; // 卡片数量
 
     var list = [];
