28fd58218591c2c1772ee4af81db67871c6e3d9e
Nesb01t <littlelumos@foxmail.com>
2022-10-12 22:44:15 +0800

    fix: username async

--- eachstar-frontend/src/api/getUserName.js
+++ eachstar-frontend/src/api/getUserName.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export function getUserName(type) {
+export async function getUserName(type) {
     var config = {
         method: "get",
         url: "server/api/user/@me?"
