4c8bb72e1ce7fbf82546e4cbe843ae44cc71e650
Nesb01t <littlelumos@foxmail.com>
2022-10-12 22:46:04 +0800

    update: loading

--- eachstar-frontend/src/views/github.vue
+++ eachstar-frontend/src/views/github.vue
@@ -159,7 +159,8 @@ export default {
      */ 
     pageChange: function (page) {
       this.currentPage = page;
-      getPageData("GitHub", this.currentPage);
+      await getPageData("GitHub", this.currentPage);
+      this.loading = false;
     },
     
     /**
