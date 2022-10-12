a194698561ba26bcab414f3efd4434b51bfbf157
Nesb01t <littlelumos@foxmail.com>
2022-10-12 19:05:03 +0800

    update: getPageData final

--- eachstar-frontend/src/api/getPageData.js
+++ eachstar-frontend/src/api/getPageData.js
@@ -15,14 +15,13 @@ export async function getPageData(type, page) {
     };
     var res = await axios(config);
     
-    console.log(res);
-    var countCard = res.data.count; // 卡片数量
+    var count = res.data.count; // 卡片数量
 
     var list = [];
     var index = 0;
     var start = page * 10;
     // 一页只刷新10个卡片 && 防止下标越界
-    while (index < 10 && start < countCard) {
+    while (index < 10 && start < count) {
         if(res.data.data[index]){
             let nowData = res.data.data[index];
             nowData.createdAt = parseTimeString(nowData.createdAt);
