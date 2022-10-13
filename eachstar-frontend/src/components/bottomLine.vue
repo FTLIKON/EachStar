53637e62c005b13edc315f1c442bd6203aec13e6
Nesb01t <littlelumos@foxmail.com>
2022-10-13 20:48:50 +0800

    update: bottom black

--- eachstar-frontend/src/components/bottomLine.vue
+++ eachstar-frontend/src/components/bottomLine.vue
@@ -26,7 +26,8 @@ export default {};
 <style lang="scss">
 .main {
   width: 100%;
-  height: 100px;
+  height: 300px;
+  margin-bottom: 80px;
   background-color: #303133;
   display: flex;
   align-items: center;
