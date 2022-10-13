2c14840b79d7262c13892697a71904a63d8e8b83
FTLIKON <854892057@qq.com>
2022-10-13 19:13:29 +0800

    update：bottomline

--- eachstar-frontend/src/components/bottomLine.vue
+++ eachstar-frontend/src/components/bottomLine.vue
@@ -1,20 +1,26 @@
 <template>
   <div class="main">
-    123
+    <div class="icpcode">互联网ICP备案：渝ICP备2022011108号</div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
+<style lang="scss">
 .main {
   bottom: 0;
   width: 100%;
   height: 30px;
   background-color: #303133;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  .icpcode {
+    margin-top: 2%;
+    margin-bottom: 2%;
+    color: #a8abb2;
+  }
 }
 </style>
