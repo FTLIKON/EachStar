af263fcaeb38e0273ad007f663e40540743c9ed7
FTLIKON <854892057@qq.com>
2022-10-13 16:03:00 +0800

    update：phone visit

--- eachstar-frontend/src/views/phone-home.vue
+++ eachstar-frontend/src/views/phone-home.vue
@@ -1,9 +1,29 @@
 <template>
-  <div>目前暂不支持手机端，请在电脑端访问！</div>
+  <div class="all">
+    <div class="logo">
+      <img class="logo-pic" src="icon.png" />
+      <span>EachStar</span>
+    </div>
+    <div>目前暂不支持手机端，请在电脑端访问！</div>
+  </div>
 </template>
 
 <script></script>
-export default {
-  name: "phonehome"
+export default { name: "phonehome" }
+<style lang="scss" scoped>
+
+
+.logo {
+  color: #ffffff;
+  font-weight: bold;
+  font-size: 35px;
+  text-shadow: 1px 1px 2px #303133, 0px 0px 3px #303133;
+  display: flex;
+  align-items: center;
 }
-<style lang="scss" scoped></style>
+.logo-pic {
+  width: 20%;
+  min-width: 100px;
+  margin-right: 2%;
+}
+</style>
