7b16e2365ba5cae2029c3373baae99dc6d5a3875
Nesb01t <littlelumos@foxmail.com>
2022-10-14 17:40:02 +0800

    update: this

--- eachstar-frontend/src/components/topNav.vue
+++ eachstar-frontend/src/components/topNav.vue
@@ -181,7 +181,7 @@ export default {
       }
 
       bus.emit("typeChange");
-      router.push('/'+type.toLowerCase())
+      router.push('/'+this.type.toLowerCase())
       await this.updateUserInfo(this.type);
       bus.emit("refreshUserInfo", this.type);
     },
