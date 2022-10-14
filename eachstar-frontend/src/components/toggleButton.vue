ac686fe3df65b930656d30f8cc9a13b16cea8738
Nesb01t <littlelumos@foxmail.com>
2022-10-14 12:03:36 +0800

    fix: commit use dark

--- eachstar-frontend/src/components/toggleButton.vue
+++ eachstar-frontend/src/components/toggleButton.vue
@@ -9,7 +9,7 @@
 import { useToggle } from '@vueuse/shared'
 import { isDark } from '../../.vitepress/theme/composables/dark'
 
-const isDark = useDark()
+// const isDark = useDark()
 const toggleDark = useToggle(isDark)
 </script>
 
