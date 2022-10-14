477c24ab0856b5a92b0f7775d0c15145b609f3fa
Nesb01t <littlelumos@foxmail.com>
2022-10-14 12:07:51 +0800

    fix: dark暂时无法更改

--- eachstar-frontend/src/components/toggleButton.vue
+++ eachstar-frontend/src/components/toggleButton.vue
@@ -9,7 +9,7 @@
 import { useToggle } from '@vueuse/shared'
 // import { isDark } from '../../.vitepress/theme/composables/dark'
 
-const isDark = useDark()
+const isDark = true
 const toggleDark = useToggle(isDark)
 </script>
 
