f8a88d543bb4f125c598ee1078d8cd9da1ab7abf
FTLIKON <854892057@qq.com>
2022-10-15 23:58:27 +0800

    update: api path

--- eachstar-frontend/vite.config.js
+++ eachstar-frontend/vite.config.js
@@ -2,7 +2,8 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
+import fs from "fs";
+import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
