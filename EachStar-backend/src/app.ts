fd4b9d8d692ab783329737b22dc3278d7bb5fbc9
FTLIKON <854892057@qq.com>
2022-10-15 17:00:11 +0800

    update: backend cors

--- EachStar-backend/src/app.ts
+++ EachStar-backend/src/app.ts
@@ -16,7 +16,22 @@ BigInt.prototype.toJSON = function () {
 }
 
 const app = new Koa()
-app.use(cors())
+app.use(
+  cors({
+    origin: function (ctx) {
+      /*if (ctx.url === '/cors') {
+          return "*"; // 允许来自所有域名请求
+      }*/
+      return '*'
+      // return 'http://localhost:8080';
+    },
+    exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+    maxAge: 5,
+    credentials: true,
+    allowMethods: ['GET', 'POST', 'DELETE'], //设置允许的HTTP请求类型
+    allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
+  }),
+)
 app.use(koaBody())
 app.use(authorization)
 app.use(getOAuthMiddleware())
