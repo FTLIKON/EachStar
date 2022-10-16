c20a5d65116e303e98c5353749ac7c9d46e1581b
FTLIKON <854892057@qq.com>
2022-10-16 09:55:23 +0800

    update: api path

--- eachstar-frontend/src/router/index.js
+++ eachstar-frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import mygithub from "../views/mygithub.vue";
 import github from "../views/github.vue";
 import mygitee from "../views/mygitee.vue";
@@ -6,7 +6,7 @@ import gitee from "../views/gitee.vue";
 import phonehome from "../views/phone-home.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
--- eachstar-frontend/vite.config.js
+++ eachstar-frontend/vite.config.js
@@ -6,6 +6,7 @@ import fs from "fs";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./", //等同于  assetsPublicPath :'./'
   plugins: [vue()],
   resolve: {
     alias: {
