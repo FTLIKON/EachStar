9bcb76abca6d930805772f9fbabc3bc98e24b660
FTLIKON <854892057@qq.com>
2022-10-21 16:46:34 +0800

    update: backend dev

--- EachStar-backend/src/app.ts
+++ EachStar-backend/src/app.ts
@@ -23,7 +23,7 @@ const app = new Koa()
 app.use(
   cors({
     origin: function (ctx) {
-      return 'https://each-star.com'
+      return 'https://localhost'
     },
     exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
     maxAge: 5,
